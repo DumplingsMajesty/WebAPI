43ac89fa82ac11c211fc46bb82d7bbb274ae33ce
餃子陛下 <lssac0925@hotmail.com>
2023-12-25 22:13:53 +0900

    Update WeatherForecastController.cs

--- WebApi002/Controllers/WeatherForecastController.cs
+++ WebApi002/Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using WebApi002;
 
 namespace WebApi002.Controllers;
 
@@ -12,22 +14,43 @@ public class WeatherForecastController : ControllerBase
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
+    private static IEnumerable<WeatherForecast> weatherForecasts;
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
     }
 
+    static WeatherForecastController()
+    {
+        if (weatherForecasts == null)
+        {
+            weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return weatherForecasts;
+    }
+
+    [HttpGet(template:"index")]
+    public WeatherForecast GetweatherForecastsByIndex(int index)
+    {
+        return weatherForecasts.ElementAt(index);
+    }
+
+    [HttpPost]
+    public WeatherForecast weatherForecastsByIndex(int index)
+    {
+        return weatherForecasts.ElementAt(index);
     }
 }
 
