a0f844c1ca67afe4b9b70381783f258c17689cff
Ning Yao <lssac0925@hotmail.com>
2023-12-26 14:49:47 +0900

    WebApi004 数据注解与数据验证

---
+++ WebApi004/WebApi004/Models/ValuesModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using WebApi004.Validations;
+
+namespace WebApi004.Models
+{
+    public class ValuesModel
+    {
+        public int Id { get; set; }
+
+        [NoSpace]
+        [DisplayName(displayName:"Name1")]
+        [MaxLength(length:5)]
+        public string Name1 { get; set; }
+
+        [Compare(nameof(Name1))]
+        public string? Name2 { get; set; }
+
+        [Range(0,200,ErrorMessage ="年龄超过范围")]
+        public int Age { get; set; }
+
+        public string? Gender { get; set; }
+    }
+}
---
+++ WebApi004/WebApi004/Validations/NoSpaceAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi004.Validations
+{
+    public class NoSpaceAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value?.ToString()?.Contains(" ")==true)
+            {
+                return new ValidationResult("该字符串不允许包含空格");
+            }
+            return ValidationResult.Success;
+        }
+
+    }
+}
