39280af7ab7079a36105b510e5e757d57779e223
Ning Yao <lssac0925@hotmail.com>
2023-12-28 13:54:30 +0900

    WebApi011 为ASP.NET Core添加后台服务

---
+++ WebApi011/WebApi011/MyBackgroundService.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Threading;
+
+namespace WebApi011
+{
+    public class MyBackgroundService : BackgroundService
+    {
+
+        private readonly ILogger<MyHostedService> logger;
+        public MyBackgroundService(ILogger<MyHostedService> logger)
+            
+        {
+            this.logger = logger;
+            logger.LogInformation(DateTime.Now.ToString() + "Before");
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            logger.LogInformation(DateTime.Now.ToString() + "start");
+
+
+            return Task.Factory.StartNew(async () =>
+            {
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    logger.LogInformation(DateTime.Now.ToString() + "......");
+                    try
+                    {
+
+                        await Task.Delay(2000, stoppingToken);
+                    }
+                    catch { }
+
+                }
+            }, stoppingToken);
+        }
+    }
+}
---
+++ WebApi011/WebApi011/MyHostedService.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.Extensions.Logging;
+
+namespace WebApi011
+{
+    public class MyHostedService : IHostedService
+    {
+
+        private readonly ILogger<MyHostedService> logger;
+
+        public MyHostedService(ILogger<MyHostedService> logger)
+        {
+
+            this.logger = logger;
+            logger.LogInformation(DateTime.Now.ToString() + "Before");
+
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation(DateTime.Now.ToString() + "start");
+
+
+            return Task.Factory.StartNew(async () =>
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    logger.LogInformation(DateTime.Now.ToString() + "......");
+                    try
+                    {
+
+                        await Task.Delay(2000, cancellationToken);
+                    }
+                    catch { }
+
+                }
+            }, cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+
+            logger.LogInformation(DateTime.Now.ToString() + "stop");
+            return Task.CompletedTask;
+        }
+    }
+}
