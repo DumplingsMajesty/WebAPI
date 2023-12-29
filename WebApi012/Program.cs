6e0b42ea4fe30f7b6e1fb8747d9ce2740088c416
餃子陛下 <lssac0925@hotmail.com>
2023-12-29 11:51:24 +0900

    WebApi012 为ASP.NET添加自定义中间件

---
+++ WebApi012/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.MapGet("/weatherforecast", () =>
+{
+    Console.WriteLine("MapGet");
+    return "Hello World";
+    Console.WriteLine("use 1 MapGet");
+});
+
+app.Use(async (context, next) =>
+{
+    var stopWatch = new Stopwatch();
+    stopWatch.Start();
+
+    Console.WriteLine("use 1 start");
+    await next();
+
+    stopWatch.Stop();
+    Console.WriteLine(stopWatch.ElapsedMilliseconds);
+})
+.Use(async (context, next) =>
+{
+    Console.WriteLine("use 2 start");
+    await next();
+    Console.WriteLine("use 2 end");
+})
+.Use(async (context, next) =>
+ {
+     Console.WriteLine("use 3 start");
+     await next();
+     Console.WriteLine("use 3 end");
+ });
+;
+
+app.Run();
+
---
+++ WebApi012/StopwatchMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace WebApi012
+{
+	public class StopwatchMiddleware
+	{
+
+		private readonly RequestDelegate next;
+
+        public StopwatchMiddleware(RequestDelegate next)
+		{
+			this.next = next;
+		}
+
+		public async Task Invoke(HttpContext httpContext)
+		{
+			var stopWatch = new Stopwatch();
+			stopWatch.Start();
+
+			await next(httpContext);
+
+			stopWatch.Stop();
+			Console.WriteLine(stopWatch.ElapsedMilliseconds);
+		}
+	}
+}
+
