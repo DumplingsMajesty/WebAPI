18ce82e728b757e81ad55e7bb3d5cc96ed230054
餃子陛下 <lssac0925@hotmail.com>
2023-12-29 14:11:45 +0900

    WebApi013 使用MAPWHEN的流水线分支

---
+++ WebApi013/Program.cs
@@ -0,0 +1,47 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/weatherforecast", () =>
+{
+    return "weatherforecast";
+});
+
+app.MapWhen(context =>
+{
+    return context.Request.Query.ContainsKey("abc");
+}, app1 =>
+{
+    app1.Run(async content =>
+    {
+        await content.Response.WriteAsync("Default");
+    });
+}
+
+);
+
+app.MapWhen(context => context.Request.Method.ToUpper() == "POST",
+    app1 =>
+    {
+        app1.MapWhen(context => context.Request.Query.ContainsKey("a1"),
+            app2 =>
+            {
+                app2.Run(async content =>
+                {
+                    await content.Response.WriteAsync("a1");
+                });
+            });
+        app1.MapWhen(context => context.Request.Query.ContainsKey("a2"),
+            app2 =>
+            {
+                app2.Run(async content =>
+                {
+                    await content.Response.WriteAsync("a2");
+                });
+            });
+    }
+
+
+);
+
+app.Run();
+
