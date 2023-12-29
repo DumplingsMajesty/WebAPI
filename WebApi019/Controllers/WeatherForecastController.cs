41e49c8ec9e6d15d2f1048c1f66fc9199375dc84
餃子陛下 <lssac0925@hotmail.com>
2023-12-29 17:41:07 +0900

    WebApi019 WebApi中的数据保护

---
+++ WebApi019/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi019.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IDataProtectionProvider dataProtectionProvider;
+
+    public WeatherForecastController(
+        ILogger<WeatherForecastController> logger,
+        IDataProtectionProvider dataProtectionProvider)
+    {
+        _logger = logger;
+        this.dataProtectionProvider = dataProtectionProvider;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        var protector = dataProtectionProvider.CreateProtector("123abc");
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast(protector)
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
+
---
+++ WebApi019/WeatherForecast.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+
+namespace WebApi019;
+
+public class WeatherForecast
+{
+
+    private IDataProtector protector;
+
+    public WeatherForecast(IDataProtector protector)
+    {
+        this.protector = protector;
+    }
+
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+
+    public string? SummaryProtector => protector.Protect(Summary!);
+
+    public string? SummaryUnProtector => protector.Unprotect(SummaryProtector!);
+
+}
+
