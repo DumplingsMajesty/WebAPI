c64a170e84eabab81bcb7f51dc4a9610ddf6c66c
餃子陛下 <lssac0925@hotmail.com>
2023-12-29 16:42:12 +0900

    WebApi016 基于Identity的身份验证

---
+++ WebApi016Console/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var url = "https://localhost:7295/Identity/Account/Login";
+
+var httpClient = new HttpClient();
+var content = httpClient.GetStringAsync(url).Result;
+
+Console.WriteLine(content);
+
+var form = new Dictionary<string, string>();
+form.Add("Input.Email", "XXXXX");
+form.Add("Input.Password", "XXXXX");
+form.Add("_RequestVerifivationToken", "XXXXX");
+form.Add("Input.RememberMe", "false");
+
+var resutl = httpClient.PostAsync(url, new FormUrlEncodedContent(form)).Result;
+Console.WriteLine(resutl);
